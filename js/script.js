d5cd0270fb7852bb9979bf644ae398c5157fcc93
bruno777202 <b.medeiros@edu.senai.br>
2026-05-13 10:52:48 -0300

    side petshop

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 // scripts petshop
+document.getElementById("formcontato").addEventListener("submit", function(e){
+    e.preventdefault()
+
+    let nome = document.getElementById("nome").value.trim()
+    let email = document.getElementById("email").value.trim()
+    let mensagem = document.getElementById("mensagem").value.trim()
+
+    if(nome == "" || email == "" || mensagem== ""){
+        alert("preencha todos os campos!")
+        return;
+    }else{
+        alert("mensagem enviada com sucesso!")
+    }
+  this.reset()
+})
+
+//******************************************************* *//
+
+let botaomenu= document.getElementById("menu-bnt")
+let menu = document.getElementById("menu");
+
+botaomenu.addEventListener("click", () => {
+menu.classList.toggle("ativo");
+})
